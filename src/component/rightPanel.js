984b441e073140b3bc56984106a4c27e7eb2625f
Liam D <liamdocuk@gmail.com>
2018-07-25 18:58:00 +0200

    Update rightPanel.js

--- src/component/rightPanel.js
+++ src/component/rightPanel.js
@@ -5,7 +5,7 @@ const rightPanel = props => {
 		const time = props.timeConvert(props.currentMessage.time_sent);
 		const subject = props.currentMessage.subject.slice(0, 1).toUpperCase() + props.currentMessage.subject.slice(1);
 
-		if(props.currentMessage !== null) {
+		if(props.currentMessage !== null && props.messages.length > 0) {
 			return(
 				<div className="rightPanel">
 					<div className="selected-container">
